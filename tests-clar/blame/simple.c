309661324c25dba44a6114d0103ec9434950b8b0
Ben Straub <bs@github.com>
2013-05-01 13:21:07 -0700

    Blame: embryonic test on a real file

--- tests-clar/blame/simple.c
+++ tests-clar/blame/simple.c
@@ -67,4 +67,89 @@ void test_blame_simple__trivial_blamerepo(void)
 	cl_git_sandbox_cleanup();
 }
 
+
+/*
+ * $ git blame -s 359fc2d -- include/git2.h
+ * d12299fe src/git.h       1) /
+ * 359fc2d2 include/git2.h  2)  * Copyright (C) the libgit2 contributors. All rights reserved.
+ * d12299fe src/git.h       3)  *
+ * bb742ede include/git2.h  4)  * This file is part of libgit2, distributed under the GNU GPL v2 with
+ * bb742ede include/git2.h  5)  * a Linking Exception. For full terms see the included COPYING file.
+ * d12299fe src/git.h       6)  /
+ * d12299fe src/git.h       7) 
+ * d12299fe src/git.h       8) #ifndef INCLUDE_git_git_h__
+ * d12299fe src/git.h       9) #define INCLUDE_git_git_h__
+ * d12299fe src/git.h      10) 
+ * 96fab093 include/git2.h 11) #include "git2/version.h"
+ * 9d1dcca2 src/git2.h     12) 
+ * 44908fe7 src/git2.h     13) #include "git2/common.h"
+ * a15c550d include/git2.h 14) #include "git2/threads.h"
+ * 44908fe7 src/git2.h     15) #include "git2/errors.h"
+ * d12299fe src/git.h      16) 
+ * 44908fe7 src/git2.h     17) #include "git2/types.h"
+ * d12299fe src/git.h      18) 
+ * 44908fe7 src/git2.h     19) #include "git2/oid.h"
+ * 638c2ca4 src/git2.h     20) #include "git2/signature.h"
+ * 44908fe7 src/git2.h     21) #include "git2/odb.h"
+ * d12299fe src/git.h      22) 
+ * 44908fe7 src/git2.h     23) #include "git2/repository.h"
+ * 44908fe7 src/git2.h     24) #include "git2/revwalk.h"
+ * bf787bd8 include/git2.h 25) #include "git2/merge.h"
+ * 0984c876 include/git2.h 26) #include "git2/graph.h"
+ * 2f8a8ab2 src/git2.h     27) #include "git2/refs.h"
+ * 27df4275 include/git2.h 28) #include "git2/reflog.h"
+ * a346992f include/git2.h 29) #include "git2/revparse.h"
+ * d12299fe src/git.h      30) 
+ * 44908fe7 src/git2.h     31) #include "git2/object.h"
+ * 44908fe7 src/git2.h     32) #include "git2/blob.h"
+ * 44908fe7 src/git2.h     33) #include "git2/commit.h"
+ * 44908fe7 src/git2.h     34) #include "git2/tag.h"
+ * 44908fe7 src/git2.h     35) #include "git2/tree.h"
+ * 65b09b1d include/git2.h 36) #include "git2/diff.h"
+ * d12299fe src/git.h      37) 
+ * 44908fe7 src/git2.h     38) #include "git2/index.h"
+ * 5d4cd003 include/git2.h 39) #include "git2/config.h"
+ * 41fb1ca0 include/git2.h 40) #include "git2/transport.h"
+ * 2dc31040 include/git2.h 41) #include "git2/remote.h"
+ * 764df57e include/git2.h 42) #include "git2/clone.h"
+ * 5280f4e6 include/git2.h 43) #include "git2/checkout.h"
+ * 613d5eb9 include/git2.h 44) #include "git2/push.h"
+ * d12299fe src/git.h      45) 
+ * 111ee3fe include/git2.h 46) #include "git2/attr.h"
+ * f004c4a8 include/git2.h 47) #include "git2/ignore.h"
+ * 111ee3fe include/git2.h 48) #include "git2/branch.h"
+ * 9c82357b include/git2.h 49) #include "git2/refspec.h"
+ * d6258deb include/git2.h 50) #include "git2/net.h"
+ * b311e313 include/git2.h 51) #include "git2/status.h"
+ * 3412391d include/git2.h 52) #include "git2/indexer.h"
+ * bfc9ca59 include/git2.h 53) #include "git2/submodule.h"
+ * bf477ed4 include/git2.h 54) #include "git2/notes.h"
+ * edebceff include/git2.h 55) #include "git2/reset.h"
+ * 743a4b3b include/git2.h 56) #include "git2/message.h"
+ * 0a32dca5 include/git2.h 57) #include "git2/pack.h"
+ * 590fb68b include/git2.h 58) #include "git2/stash.h"
+ * bf477ed4 include/git2.h 59) 
+ * d12299fe src/git.h      60) #endif
+ */
+void test_blame_simple__trivial_libgit2(void)
+{
+	git_repository *repo;
+	git_blame *blame;
+	git_blame_options opts = GIT_BLAME_OPTIONS_INIT;
+	git_object *obj;
+
+	cl_git_pass(git_repository_open(&repo, cl_fixture("../..")));
+
+	cl_git_pass(git_revparse_single(&obj, repo, "359fc2d"));
+	git_oid_cpy(&opts.newest_commit, git_object_id(obj));
+	git_object_free(obj);
+
+	cl_git_pass(git_blame_file(&blame, repo, "include/git2.h", &opts));
+
+	check_blame_hunk_index(blame, 0,  1, 4, "da237394e6132d20d30f175b9b73c8638fddddda");
+
+	git_blame_free(blame);
+	git_repository_free(repo);
+}
+
 /* TODO: no newline at end of file? */
